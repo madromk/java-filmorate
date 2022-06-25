e2814b66c7e1912cd1e14a6d4a9132e796214136
Roman <maderomk@yandex.ru>
2022-06-25 17:23:57 +0300

    у методов в параметрах тип данных String заменён на int

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface FilmStorage {
-    Film getFilmById(String id);
+    Film getFilmById(int id);
     List<Film> findAllFilms();
     void addFilm(Film film);
     void updateFilm(Film film);
 
-    boolean isContainsFilms(String id);
+    boolean isContainsFilms(int id);
 
 }
