c95db6dd37ad4cf40ac966b1c6041958a42c7311
Roman <maderomk@yandex.ru>
2022-06-25 17:16:54 +0300

    в методе getPopularFilms компаратор вынесен в отдельный метод, и результат сразу возвращается

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,46 +1,69 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class FilmService {
     private final FilmStorage filmStorage;
 
-    public FilmService() {
-        this.filmStorage = new InMemoryFilmStorage();
+    @Autowired
+    public FilmService(InMemoryFilmStorage fileStorage) {
+        this.filmStorage = fileStorage;
     }
 
-    public void addLike(String filmId, String userId) {
+    public void addLike(int filmId, int userId) {
         Film film = filmStorage.getFilmById(filmId);
-        film.addLike(Integer.parseInt(userId));
+        film.getAmountLikes().add(userId);
         filmStorage.updateFilm(film);
     }
 
-    public void removeLike(String filmId, String userId) {
+    public void removeLike(int filmId, int userId) {
         Film film = filmStorage.getFilmById(filmId);
-        film.removeLike(Integer.parseInt(userId));
+        film.getAmountLikes().remove(userId);
         filmStorage.updateFilm(film);
     }
 
     public List<Film> getPopularFilms(String count) {
-        int amount = Integer.parseInt(count);
-        List<Film> result = filmStorage.findAllFilms().stream()
+        return filmStorage.findAllFilms().stream()
                 .filter(film -> film.getAmountLikes() != null)
-                .sorted(Comparator.comparing(film -> film.getAmountLikes().size(), Comparator.reverseOrder()))
-                .limit(amount)
+                .sorted(sortPopularFilm())
+                .limit(Integer.parseInt(count))
                 .collect(Collectors.toList());
-        return result;
+    }
+
+    public Comparator<Film> sortPopularFilm() {
+        return Comparator.comparing(film -> film.getAmountLikes().size(), Comparator.reverseOrder());
+    }
+    public Film getFilmById(int id) {
+        return filmStorage.getFilmById(id);
+    }
+    public List<Film> findAllFilms() {
+        return filmStorage.findAllFilms();
+    }
+
+    public void addFilm(Film film) {
+        filmStorage.addFilm(film);
+    }
+    public void updateFilm(Film film) {
+        filmStorage.updateFilm(film);
+    }
+    public boolean isContainsFilms(int id) {
+        return filmStorage.isContainsFilms(id);
     }
 
     public FilmStorage getFilmStorage() {
         return filmStorage;
     }
+
+
 }
